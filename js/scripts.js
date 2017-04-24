0dbc306686faead7eed916c4875d173aadbc99e0
Brian Rush and Daniela Trulls <brian@>
2017-04-24 13:51:49 -0700

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ alert ((weight / Math.pow (height, 2)) * 703);
 };
 
 bmi(height, weight);
+
+//Calculate Celcius/ fahrenheit convertion
+
+var celcius = parseFloat(prompt("What is the temperature outside in Celcius"));
+
+var fahrenheit = function(celcius){
+	alert("the temperature in Celcius is " + (celcius*(9/5)+32) + " C.");
+};
+
+fahrenheit(celcius);
